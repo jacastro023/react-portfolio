52713dd0175fae0601cfab2782ad35ebd8904607
Jose Castro <jacastro023@gmail.com>
2022-03-09 15:33:06 -0600

    made navbar menu dropdown

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 import Pdf from "../resumes/resume.pdf"
 
 
@@ -9,6 +9,8 @@ import Pdf from "../resumes/resume.pdf"
 function Header() {
 
   return (
+    <div>
+      <div className="nav1">
     <Navbar bg="dark" variant="dark">
     <Container>
     <Navbar.Brand href="/">JOSE C</Navbar.Brand>
@@ -20,6 +22,25 @@ function Header() {
     </Nav>
     </Container>
   </Navbar>
+  </div>
+    <div className="nav2">
+    <Navbar variant="dark" bg="dark" expand="lg">
+      <Container fluid>
+      <Navbar.Brand href="/">JOSE C</Navbar.Brand>
+            <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="Menu"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item href="/">Home</NavDropdown.Item>
+              <NavDropdown.Item href="/projects">Projects</NavDropdown.Item>
+              <NavDropdown.Item href="/contact">Contact</NavDropdown.Item>
+              <NavDropdown.Item href = {Pdf} target = "_blank">Resume</NavDropdown.Item>
+            </NavDropdown>
+      </Container>
+    </Navbar>
+    </div>
+    </div>
   );
 }
 
