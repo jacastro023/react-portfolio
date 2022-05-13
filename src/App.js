a01a12a3820b3f0f5b16c096bbc71098a61505a3
Jose Castro <jacastro023@gmail.com>
2022-05-13 12:13:23 -0500

    adjusted links for smoother transit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,31 @@
 import Header from "./components/Header";
 import Footer from "./components/footer/index.js";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Wrapper from "./components/Wrapper";
 import Home from "./pages/home.js";
 import Contact from "./pages/contact.js";
 import Projects from "./pages/projects.js";
-import BarLoader from "react-spinners/BarLoader";
-import React, {useState, useEffect} from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
- 
-  const [loading, setLoading] = useState(false)
-
-  useEffect(()=>{
-     setLoading(true);
-     setTimeout(()=> {
-       setLoading(false);
-      }, 1000);
-  }, []);
-
   return (
+    <div>
       <div>
-        {loading ? (
-          <div className="App">
-          <BarLoader color={"#010101"} loading={loading}  size={150} />
-          </div>
-        ) : (
-          <div>
         <Header />
         <Wrapper>
-        <Routes>
-          <Route exact path="/" element={<Home />}/>
-          <Route exact path="/Home" element={<Home />}/>
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="/projects" element={<Projects />} />
-        </Routes>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/Home" element={<Home />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/projects" element={<Projects />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
         </Wrapper>
         <Footer />
-        </div>
-        )}
       </div>
-  )};
+    </div>
+  );
+}
 
 export default App;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,32 @@
 import React from "react";
 import "./style.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
-import Pdf from "../resumes/resume.pdf"
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import Pdf from "../resumes/resume.pdf";
+import { Link } from "react-router-dom";
 
 function Header() {
-
   return (
     <div>
       <div className="nav1">
-    <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="/">JOSE C</Navbar.Brand>
-    <Nav>
-      <Nav.Link href="/">Home</Nav.Link>
-      <Nav.Link href="/projects">Projects</Nav.Link>
-      <Nav.Link href="/contact">Contact</Nav.Link>
-      <Nav.Link href = {Pdf} target = "_blank">Resume</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>
-  </div>
-    <div className="nav2">
-    <Navbar variant="dark" bg="dark" expand="lg">
-      <Container fluid>
-      <Navbar.Brand href="/">JOSE C</Navbar.Brand>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="/">JOSE C</Navbar.Brand>
+            <Nav>
+              <Link to="/">Home</Link>
+              <Link to="/projects">projects</Link>
+              <Link to="/contact">Contact</Link>
+              <Link to={Pdf} target="_blank">
+                Resume
+              </Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
+      <div className="nav2">
+        <Navbar variant="dark" bg="dark" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="/">JOSE C</Navbar.Brand>
             <NavDropdown
               id="nav-dropdown-dark-example"
               title="Menu"
@@ -35,11 +35,13 @@ function Header() {
               <NavDropdown.Item href="/">Home</NavDropdown.Item>
               <NavDropdown.Item href="/projects">Projects</NavDropdown.Item>
               <NavDropdown.Item href="/contact">Contact</NavDropdown.Item>
-              <NavDropdown.Item href = {Pdf} target = "_blank">Resume</NavDropdown.Item>
+              <NavDropdown.Item href={Pdf} target="_blank">
+                Resume
+              </NavDropdown.Item>
             </NavDropdown>
-      </Container>
-    </Navbar>
-    </div>
+          </Container>
+        </Navbar>
+      </div>
     </div>
   );
 }
